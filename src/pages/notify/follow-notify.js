6684c6af27d8fb2ac49ed76ea4da51111a77126e
tianlu1677 <tianlu1677@gmail.com>
2020-08-14 16:06:56 +0800

    add follow

--- src/pages/notify/follow-notify.js
+++ src/pages/notify/follow-notify.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {SafeAreaView, StyleSheet, ScrollView, View, Text, Button} from 'react-native';
 import styled from 'styled-components/native';
 import goPage from '../../utils/page_path';
+import AccountFollow from './components/account-follow';
 
 class FollowNotify extends Component {
   constructor(props) {
@@ -22,7 +23,11 @@ class FollowNotify extends Component {
   render() {
     return (
       <View>
-        <SafeAreaView>CommentNotify</SafeAreaView>
+        <SafeAreaView style={{backgroundColor: 'white', height: '100%'}}>
+          {[1, 2, 3].map(item => {
+            return <AccountFollow account={{}} actionText={'你好'} time={'1小时前'} key={item} />;
+          })}
+        </SafeAreaView>
       </View>
     );
   }
