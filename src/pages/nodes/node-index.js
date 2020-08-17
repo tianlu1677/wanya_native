30781348644375da2197b77ee95a5719612933fa
lijuanjuan <lijuanjuan@qipeng.com>
2020-08-18 00:02:37 +0800

    node style

--- src/pages/nodes/node-index.js
+++ src/pages/nodes/node-index.js
@@ -1,15 +1,16 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
+import {View, Text, ScrollView, StyleSheet, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
 import {getCategoryList} from '@/api/category_api';
 import {getNodeIndex} from '@/api/node_api';
 import Loading from '@/components/Loading';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
 
 const NodeIndex = () => {
   const [categories, setCategories] = useState(null);
   const [nodes, setNodes] = useState(null);
   const [layoutList, setLayoutList] = useState([]);
+  const [active, setActive] = useState(0);
 
   const scrollRef = useRef(null);
 
@@ -29,6 +30,7 @@ const NodeIndex = () => {
   };
 
   const onChange = index => {
+    setActive(index);
     scrollRef.current.scrollTo({y: layoutList[index].y, animated: true});
   };
 
@@ -38,16 +40,17 @@ const NodeIndex = () => {
 
   return categories && nodes ? (
     <NodeView>
-      <NodeNameView>
+      <View style={styles.nodeWrap}>
         {categories.map((categorie, index) => (
           <TouchableOpacity
             key={index}
             onPress={() => onChange(index)}
-            style={{padding: 20, backgroundColor: 'green', marginBottom: 10}}>
+            style={[styles.nodeName, active === index && styles.nodeNameActive]}>
             <Text>{categorie.name}</Text>
+            {active === index && <Text style={styles.active} />}
           </TouchableOpacity>
         ))}
-      </NodeNameView>
+      </View>
       <ScrollView ref={scrollRef}>
         {categories.map((categorie, index) => (
           <View key={categorie.id} onLayout={e => setLayout(e.nativeEvent.layout, index)}>
@@ -70,13 +73,38 @@ const NodeIndex = () => {
   );
 };
 
+const styles = StyleSheet.create({
+  nodeWrap: {},
+  nodeName: {
+    width: 75,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 14,
+    position: 'relative',
+    marginBottom: 10,
+  },
+  nodeNameActive: {
+    backgroundColor: 'white',
+  },
+  active: {
+    width: 4,
+    height: 10,
+    position: 'absolute',
+    left: 0,
+    top: '50%',
+    marginTop: -5,
+    backgroundColor: '#ffff00',
+  },
+});
+
 const NodeView = styled(View)`
   flex: 1;
   flex-direction: row;
 `;
 
 const NodeNameView = styled(View)`
-  width: 100px;
+  // width: 100px;
   background: pink;
 `;
 
