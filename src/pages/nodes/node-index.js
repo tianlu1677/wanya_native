e38de9f2d0348d0c62b0df3a5fd2c5e7cd83f9ef
lijuanjuan <lijuanjuan@qipeng.com>
2020-08-17 17:51:00 +0800

    node

--- src/pages/nodes/node-index.js
+++ src/pages/nodes/node-index.js
@@ -1,26 +1,85 @@
-import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  Button,
-} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
+import styled from 'styled-components/native';
+import {getCategoryList} from '@/api/category_api';
+import {getNodeIndex} from '@/api/node_api';
+import Loading from '@/components/Loading';
 
-class NodeIndex extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modalVisible: false,
-      coin_data: [],
-      loading: false,
-    };
-  }
+const NodeIndex = () => {
+  const [categories, setCategories] = useState(null);
+  const [nodes, setNodes] = useState(null);
+  const [layoutList, setLayoutList] = useState([]);
 
-  render() {
-    return <View><Text>NodeInex</Text></View>
-  }
-}
+  const scrollRef = useRef(null);
+
+  const loadData = async () => {
+    const category = await getCategoryList();
+    const node = await getNodeIndex();
+    setCategories(category);
+    setNodes(node);
+  };
+
+  const setLayout = (layout, index) => {
+    layoutList[index] = layout;
+    const list = JSON.parse(JSON.stringify(layoutList));
+    setLayoutList(list);
+  };
+
+  const onChange = index => {
+    scrollRef.current.scrollTo({y: layoutList[index].y, animated: true});
+  };
+
+  useEffect(() => {
+    loadData();
+  });
+
+  return categories && nodes ? (
+    <NodeView>
+      <NodeNameView>
+        {categories.map((categorie, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => onChange(index)}
+            style={{padding: 20, backgroundColor: 'green', marginBottom: 10}}>
+            <Text>{categorie.name}</Text>
+          </TouchableOpacity>
+        ))}
+      </NodeNameView>
+      <ScrollView ref={scrollRef}>
+        {categories.map((categorie, index) => (
+          <View key={categorie.id} onLayout={e => setLayout(e.nativeEvent.layout, index)}>
+            <Text style={{padding: 20, marginBottom: 10}}>标题是：：：：：{categorie.name}</Text>
+            <View style={{backgroundColor: 'pink'}}>
+              {nodes
+                .filter(v => v.category_id === categorie.id)
+                .map(node => (
+                  <Text key={node.id} style={{backgroundColor: 'pink', padding: 10}}>
+                    {node.name}
+                  </Text>
+                ))}
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+    </NodeView>
+  ) : (
+    <Loading />
+  );
+};
+
+const NodeView = styled(View)`
+  flex: 1;
+  flex-direction: row;
+`;
+
+const NodeNameView = styled(View)`
+  width: 100px;
+  background: pink;
+`;
+
+const NodeContent = styled(View)`
+  width: 100px;
+  background: pink;
+`;
 
 export default NodeIndex;
