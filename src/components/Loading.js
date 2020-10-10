0cca167f4838abd9d5418ddb7d882dfbfb69051b
tianlu1677 <tianlu1677@gmail.com>
2020-10-10 19:10:41 +0800

    add loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import {View, StyleSheet, ActivityIndicator} from 'react-native';
+import Spinner from 'react-native-spinkit';
 
 const Loading = () => {
+  const allTypes = ['CircleFlip', 'Bounce', 'Wave', 'WanderingCubes', 'Pulse', 'ChasingDots', 'ThreeBounce', 'Circle', '9CubeGrid', 'WordPress', 'FadingCircle', 'FadingCircleAlt']
+  const type = Math.random(allTypes.length - 1).toFixed(1) * 10
   return (
     <View style={styles.loading}>
-      <ActivityIndicator size="large" />
+      {/*<ActivityIndicator size="large" />*/}
+      <Spinner style={{}} isVisible={true} size={100} type={allTypes[type]} color={'red'} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   loading: {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
     flex: 1,
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    zIndex: 999,
   },
 });
 
