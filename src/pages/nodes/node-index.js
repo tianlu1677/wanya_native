f35e9af9b363683648490a9531a09c1261df5066
lijuanjuan <lijuanjuan@qipeng.com>
2020-11-20 18:47:46 +0800

    commit

--- src/pages/nodes/node-index.js
+++ src/pages/nodes/node-index.js
@@ -1,10 +1,18 @@
 import React, {useLayoutEffect} from 'react';
 import {Text, StyleSheet, Pressable} from 'react-native';
+import {useSelector} from 'react-redux';
 import Toast from '@/components/Toast';
 import NodeIndexComponent from '@/components/NodeIndex';
+import PushUtil from '@/utils/umeng_analytics_util';
 
 const NodeIndex = ({navigation}) => {
+  const currentAccount = useSelector(state => state.account.currentBaseInfo);
+
   const onCreateNode = () => {
+    PushUtil.onEventObject('click_create_node', {
+      account_id: currentAccount.id,
+      account_nickname: currentAccount.nickname,
+    });
     Toast.show('敬请期待');
   };
 
