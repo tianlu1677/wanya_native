bf202990813274d703107fb08b7fe2aa8c91c3f0
tianlu1677 <tianlu1677@gmail.com>
2020-12-03 18:49:42 +0800

    fixed notice params

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ import {ImageList} from '@/utils/default-image';
 import {prosettings} from '@/api/settings_api';
 import {syncDeviceToken, callbackNotification} from '@/api/app_device_api';
 import NetworkErrorModal from '@/components/NetworkErrorModal';
-import PushUtil from '@/utils/umeng_push_util'
+import PushUtil from '@/utils/umeng_push_util';
 
 import * as RootNavigation from '@/navigator/root-navigation';
 
@@ -78,12 +78,12 @@ class App extends Component {
       allowFontScaling: false,
     });
 
-    PushUtil.addTag('normal',(code,remain) =>{
-      console.log('code1', code, remain)
+    PushUtil.addTag('normal', (code, remain) => {
+      // console.log('code1', code, remain);
       // Alert.alert(`${code} ${remain}`)
-    })
+    });
     // PushUtil.addAlias('dddd', 'login_user',(code) =>{
-    //   console.log('alias', code)      
+    //   console.log('alias', code)
     // })
   }
 
@@ -120,7 +120,7 @@ class App extends Component {
   // 通知相关内容
   onRegister = response => {
     console.log('onRegister', response.token);
-    const data = {device_token: response.token, platform: response.os}
+    const data = {device_token: response.token, platform: response.os};
     syncDeviceToken(data);
   };
 
@@ -131,21 +131,29 @@ class App extends Component {
     try {
       console.log('onNotification:', notification);
       const auth_token = await Helper.getData('auth_token');
-      // if (!auth_token) {
-      //   return;
-      // }
+      if (!auth_token) {
+        return;
+      }
+      let screen = '';
+      let params = '';
       const data = notification.data;
-      const params = data.params;
-      const screen = data.screen;
+      params = data.params;
+      screen = data.screen;
+      const extra = data.extra;
+      if (!params) {
+        params = extra.params;
+      }
+      if (!screen) {
+        screen = extra.screen;
+      }
       if (!params || !screen) {
         return;
       }
       const screen_params = queryString.parse(data.params, {parseNumbers: true});
-      // debugger
       console.log('params', params, screen);
       setTimeout(() => {
         RootNavigation.navigate(data.screen, screen_params);
-      }, 1500)
+      }, 1500);
     } catch (e) {
       console.log('error', e);
     }
@@ -158,8 +166,8 @@ class App extends Component {
   loadNetworkInfo = () => {
     this.networdunsubscribe = NetInfo.addEventListener(state => {
       // console.log('state', state)
-      if(this.state.netInfoErr === !state.isConnected ) {
-        return
+      if (this.state.netInfoErr === !state.isConnected) {
+        return;
       }
       if (state.isConnected) {
         this.setState({
