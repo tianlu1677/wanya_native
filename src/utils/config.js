4627bb7ae61240423867f5a2db1098131cbd57da
tianlu1677 <tianlu1677@gmail.com>
2020-12-04 15:34:38 +0800

    add sentry error collect

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,3 +5,8 @@ export const BaseProApi = 'https://meirixinxue.com/';//'https://meirixinxue.com/
 export const BaseDevApi = 'https://xinxue.meirixinxue.com';
 
 export const BaseApiUrl = __DEV__ ? BaseDevApi : BaseProApi;
+
+// sentry 错误收集
+export const BaseDevSentryURL = 'https://4a1adfee8b7e49dc91f3a0de7dfcde82@sentry.meirixinxue.com/5';
+export const BaseProSentryURL = 'https://4a1adfee8b7e49dc91f3a0de7dfcde82@sentry.meirixinxue.com/5';
+export const BaseSentryURL = __DEV__ ? BaseDevSentryURL : BaseProSentryURL;
