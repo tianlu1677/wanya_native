05c986c0f651253f132f84e0e217a24a393d9452
tianlu1677 <tianlu1677@gmail.com>
2020-12-08 17:05:45 +0800

    fixed view

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -6,7 +6,7 @@ export const NAV_BAR_HEIGHT_IOS = 44; //导航栏在iOS中的高度
 export const NAV_BAR_HEIGHT_ANDROID = 50; //导航栏在Android中的高度
 export const NAV_BAR_HEIGHT = Platform.OS === 'ios' ? NAV_BAR_HEIGHT_IOS : NAV_BAR_HEIGHT_ANDROID;
 
-export const NAVIGATION_BAR_HEIGHT = NAV_BAR_HEIGHT + getStatusBarHeight();
+export const NAVIGATION_BAR_HEIGHT = NAV_BAR_HEIGHT;
 export const STATUS_BAR_HEIGHT = Platform.OS !== 'ios' || !isIphoneX ? getStatusBarHeight() : 20;
 export const BOTTOM_HEIGHT = Platform.OS === 'ios' ? getBottomSpace() : 0;
 export const IsIos = Platform.OS === 'ios'
@@ -15,6 +15,8 @@ export const BASIC_HEIGHT = BOTTOM_HEIGHT > 0 ? BOTTOM_HEIGHT : STATUS_BAR_HEIGH
 
 export const SCALE = Dimensions.get('window').width / 375 > 1 ? 1.08 : 1;
 
+//padding的距离
+export const PADDING_TOP = Platform.OS === 'ios' ? 20 : 0;
 
 console.log('getStatusBarHeight();', getStatusBarHeight())
 console.log('getBottomSpace();', getBottomSpace())
