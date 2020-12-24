db8d17fcf929e5851f2ff196352ba80761930bc6
tianlu1677 <tianlu1677@gmail.com>
2020-12-24 17:57:43 +0800

    fixed navbar

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -2,22 +2,22 @@ import React, {useState, useEffect, useLayoutEffect, useReducer} from 'react';
 import {Platform, Dimensions} from 'react-native';
 import {isIphoneX, getStatusBarHeight, getBottomSpace} from 'react-native-iphone-x-helper';
 
+export const IsIos = Platform.OS === 'ios';
 export const NAV_BAR_HEIGHT_IOS = 44; //导航栏在iOS中的高度
 export const NAV_BAR_HEIGHT_ANDROID = 50; //导航栏在Android中的高度
-export const NAV_BAR_HEIGHT = Platform.OS === 'ios' ? NAV_BAR_HEIGHT_IOS : NAV_BAR_HEIGHT_ANDROID;
+export const NAV_BAR_HEIGHT = IsIos ? NAV_BAR_HEIGHT_IOS : NAV_BAR_HEIGHT_ANDROID;
 
-export const STATUS_BAR_HEIGHT = Platform.OS !== 'ios' || !isIphoneX ? getStatusBarHeight(false) : getStatusBarHeight(false);
-export const BOTTOM_HEIGHT = Platform.OS === 'ios' ? getBottomSpace() : 0;
-export const IsIos = (Platform.OS === 'ios')
+export const STATUS_BAR_HEIGHT = !IsIos || !isIphoneX ? getStatusBarHeight(false) : getStatusBarHeight(false);
+export const BOTTOM_HEIGHT = IsIos ? getBottomSpace() : 0;
 
 export const BASIC_HEIGHT = BOTTOM_HEIGHT > 0 ? BOTTOM_HEIGHT : STATUS_BAR_HEIGHT;
 
 export const SCALE = Dimensions.get('window').width / 375 > 1 ? 1.08 : 1;
 
 //padding的距离
-export const PADDING_TOP = Platform.OS === 'ios' ? 20 : 0;
-export const SAFE_TOP = Platform.OS === 'ios' ? getStatusBarHeight(true) : 0;
-export const UNSAFE_TOP = Platform.OS === 'ios' ? getStatusBarHeight(false) : 0;
+export const PADDING_TOP = IsIos ? 20 : 0;
+export const SAFE_TOP = IsIos ? getStatusBarHeight(true) : 0;
+export const UNSAFE_TOP = IsIos ? getStatusBarHeight(false) : 0;
 
 // 设备的宽高
 export const SCREEN_WIDTH = Dimensions.get('window').width
