f3d7f4890429f894eaad656a561070a4da02fcd1
tianlu1677 <tianlu1677@gmail.com>
2021-02-02 19:00:54 +0800

    split sentry url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,4 @@
+import {Platform} from 'react-native';
 // API 的相关配置
 // export const BaseProApi = 'https://meirixinxue.com/'; //'https://meirixinxue.com/';
 export const BaseProApi = 'https://xinxue.meirixinxue.com/'; //'https://meirixinxue.com/';
@@ -10,7 +11,9 @@ export const BaseApiUrl = __DEV__ ? BaseDevApi : BaseProApi;
 export const WANYA_VERSION = '0.0.12';
 // sentry 错误收集
 export const BaseDevSentryURL = 'https://4a1adfee8b7e49dc91f3a0de7dfcde82@sentry.meirixinxue.com/5';
-export const BaseProSentryURL = 'https://51fe408bc69c413989f6c84304eb13db@sentry.meirixinxue.com/6';
+const BaseProSentryIOSURL = 'https://51fe408bc69c413989f6c84304eb13db@sentry.meirixinxue.com/6';
+const BaseProSentryAndroidURL = 'http://a15165bfa1514bcfbfc94e2ad5b9b956@sentry.meirixinxue.com/7';
+const BaseProSentryURL = Platform.OS === 'ios' ? BaseProSentryIOSURL : BaseProSentryAndroidURL;
 export const BaseSentryURL = __DEV__ ? BaseDevSentryURL : BaseProSentryURL;
 
 // 高德定位的key
