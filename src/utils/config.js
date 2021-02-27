233edc2d1c01c5768d9d139537951a3b3536f2fc
tianlu1677 <tianlu1677@gmail.com>
2021-02-27 19:55:35 +0800

    use dev api

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,8 +2,8 @@ import {Platform} from 'react-native';
 // API 的相关配置
 export const BaseProApi = 'https://meirixinxue.com/'; //'https://meirixinxue.com/';
 // export const BaseProApi = 'https://xinxue.meirixinxue.com/'; //'https://meirixinxue.com/';
-export const BaseDevApi = 'https://meirixinxue.com/';
-// export const BaseDevApi = 'https://xinxue.meirixinxue.com';
+// export const BaseDevApi = 'https://meirixinxue.com/';
+export const BaseDevApi = 'https://xinxue.meirixinxue.com';
 
 export const BaseApiUrl = __DEV__ ? BaseDevApi : BaseProApi;
 
