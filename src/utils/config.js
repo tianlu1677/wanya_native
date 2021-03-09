c90045b629549ba0c48c04a58c0b633e2cfeb3cc
tianlu1677 <tianlu1677@gmail.com>
2021-03-09 17:39:49 +0800

    change config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,14 +1,14 @@
 import {Platform} from 'react-native';
 // API 的相关配置
-export const BaseProApi = 'https://meirixinxue.com/'; //'https://meirixinxue.com/';
-// export const BaseProApi = 'https://xinxue.meirixinxue.com/'; //'https://meirixinxue.com/';
+// export const BaseProApi = 'https://meirixinxue.com/'; //'https://meirixinxue.com/';
+export const BaseProApi = 'https://xinxue.meirixinxue.com/'; //'https://meirixinxue.com/';
 // export const BaseDevApi = 'https://meirixinxue.com/';
 export const BaseDevApi = 'https://xinxue.meirixinxue.com';
 
 export const BaseApiUrl = __DEV__ ? BaseDevApi : BaseProApi;
 
 //标识当前版本
-export const WANYA_VERSION = '0.0.14';
+export const WANYA_VERSION = '0.0.15';
 // sentry 错误收集
 export const BaseDevSentryURL = 'https://4a1adfee8b7e49dc91f3a0de7dfcde82@sentry.meirixinxue.com/5';
 const BaseProSentryIOSURL = 'https://51fe408bc69c413989f6c84304eb13db@sentry.meirixinxue.com/6';
