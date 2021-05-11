33f424fecce6df8b7ea7538986327a4f82d9b534
lijuanjuan <lijuanjuan@qipeng.com>
2021-05-11 18:18:47 +0800

    onpress

--- src/components/Item/base-topic.js
+++ src/components/Item/base-topic.js
@@ -174,7 +174,6 @@ const styles = StyleSheet.create({
   imageMultiWrapper: {
     flexDirection: 'row',
     flexWrap: 'wrap',
-    backgroundColor: 'pink',
   },
   imageMulti: {
     width: imageMultiWidth,
