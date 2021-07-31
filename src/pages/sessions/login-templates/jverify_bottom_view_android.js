bf6d67fb5e7be37dbaa4a087e5cb234e413c01fa
tianlu1677 <tianlu1677@gmail.com>
2021-08-01 01:26:12 +0800

    fixed android view

--- src/pages/sessions/login-templates/jverify_bottom_view_android.js
+++ src/pages/sessions/login-templates/jverify_bottom_view_android.js
@@ -13,107 +13,107 @@ import Toast from '@/components/Toast';
 import ShareUtil from '@/utils/umeng_share_util';
 import {SignInWithAppleButton} from '@/components/AppleLogin';
 import {store} from '@/redux/stores/store';
-import {IsIos} from '@/utils/navbar';
+import {IsIos, SCREEN_WIDTH} from '@/utils/navbar';
 import Helper from "@/utils/helper"
 
 const JveryfyBottomViewAndroid = ({}) => {
 
   //{"accessToken": "2D07206AFCBD395D8C6E13572734266E", "city": "海淀", "expiration": null, "gender": "男", "iconurl": "https://thirdqq.qlogo.cn/g?b=oidb&k=37YbkEGP192zF3YTbvzR4A&s=100&t=1556440734", "name": "狂奔的蜗牛", "openid": "2F942F4D00671AE32030DE17B870EBCA", "province": "北京", "uid": "2F942F4D00671AE32030DE17B870EBCA"}
-  const qqLogin = async () => {
-    try {
-      ShareUtil.auth(0, async (code, result, message) => {
-        console.log('res', code, result, message);
-        if (code === 200) {
-          let gender = result.gender === '男' ? 'man' : 'woman';
-          let signData = {
-            gender: gender,
-            city: result.city,
-            avatar_url: result.iconurl,
-            nickname: result.name,
-            openid: result.openid,
-            province: result.province,
-            unionid: result.uid,
-            provider: 'qq',
-          };
-          const res = await appQqSignIn(signData);
-          afterLoginSuccess(res, signData);
-        } else {
-          Toast.showError('获取QQ信息失败，请稍后再试');
-        }
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  // {"accessToken": "2.00okeQaCAAunBE6c79b21cd0chvo4D", "city": "8", "expiration": null, "gender": "男", "iconurl": "https://tva1.sinaimg.cn/crop.0.0.180.180.180/8d279d76jw1e8qgp5bmzyj2050050aa8.jpg?KID=imgbed,tva&Expires=1625466852&ssig=ttOGekAaG9", "name": "风前无止境", "province": "50", "refreshToken": "2.00okeQaCAAunBEfa3e742d05kRjUfE", "uid": "2368183670", "unionid": "2368183670"}
-  const weiboLogin = async () => {
-    try {
-      ShareUtil.auth(1, async (code, result, message) => {
-        console.log('weiboLogin res', code, result, message);
-        if (code === 200) {
-          let gender = result.gender === '男' ? 'man' : 'woman';
-          let signData = {
-            gender: gender,
-            city: result.city,
-            avatar_url: result.iconurl,
-            nickname: result.name,
-            openid: result.unionid,
-            province: result.province,
-            unionid: result.uid,
-            provider: 'weibo',
-          };
-          const res = await appWeiboSignIn(signData);
-          afterLoginSuccess(res, signData);
-        } else {
-          Toast.showError('获取微博信息失败，请稍后再试');
-        }
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const wechatLogin = async () => {
-    try {
-      const codeRes = await WeChat.sendAuthRequest('snsapi_userinfo');
-      let data = {
-        code: codeRes.code,
-        app_id: codeRes.appid || 'wx17b69998e914b8f0',
-      };
-      console.log('codeRes', codeRes)
-      const res = await appWechatSignIn(data);
-      const signData = {
-        ...res.account,
-        provider: 'wechat',
-      }
-      console.log('res', res);
-      afterLoginSuccess(res, signData);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  // 苹果登录的请求
-  const appleSignIn = async result => {
-    const {fullName, identityToken, user, email} = result;
-    console.log('result', result);
-    try {
-      const data = {
-        email: email,
-        user_id: user,
-        identity_token: identityToken,
-        nickname: fullName.nickname || fullName.failyName,
-        provider: 'apple',
-        unionid: user,
-      };
-      const res = await appAppleSignIn(data);
-      afterLoginSuccess(res, data);
-    } catch (e) {
-      console.log('e', e);
-      Toast.showError('苹果登录失败, 请稍后重试。');
-    }
-  };
+  // const qqLogin = async () => {
+  //   try {
+  //     ShareUtil.auth(0, async (code, result, message) => {
+  //       console.log('res', code, result, message);
+  //       if (code === 200) {
+  //         let gender = result.gender === '男' ? 'man' : 'woman';
+  //         let signData = {
+  //           gender: gender,
+  //           city: result.city,
+  //           avatar_url: result.iconurl,
+  //           nickname: result.name,
+  //           openid: result.openid,
+  //           province: result.province,
+  //           unionid: result.uid,
+  //           provider: 'qq',
+  //         };
+  //         const res = await appQqSignIn(signData);
+  //         afterLoginSuccess(res, signData);
+  //       } else {
+  //         Toast.showError('获取QQ信息失败，请稍后再试');
+  //       }
+  //     });
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
+  //
+  // // {"accessToken": "2.00okeQaCAAunBE6c79b21cd0chvo4D", "city": "8", "expiration": null, "gender": "男", "iconurl": "https://tva1.sinaimg.cn/crop.0.0.180.180.180/8d279d76jw1e8qgp5bmzyj2050050aa8.jpg?KID=imgbed,tva&Expires=1625466852&ssig=ttOGekAaG9", "name": "风前无止境", "province": "50", "refreshToken": "2.00okeQaCAAunBEfa3e742d05kRjUfE", "uid": "2368183670", "unionid": "2368183670"}
+  // const weiboLogin = async () => {
+  //   try {
+  //     ShareUtil.auth(1, async (code, result, message) => {
+  //       console.log('weiboLogin res', code, result, message);
+  //       if (code === 200) {
+  //         let gender = result.gender === '男' ? 'man' : 'woman';
+  //         let signData = {
+  //           gender: gender,
+  //           city: result.city,
+  //           avatar_url: result.iconurl,
+  //           nickname: result.name,
+  //           openid: result.unionid,
+  //           province: result.province,
+  //           unionid: result.uid,
+  //           provider: 'weibo',
+  //         };
+  //         const res = await appWeiboSignIn(signData);
+  //         afterLoginSuccess(res, signData);
+  //       } else {
+  //         Toast.showError('获取微博信息失败，请稍后再试');
+  //       }
+  //     });
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
+  //
+  // const wechatLogin = async () => {
+  //   try {
+  //     const codeRes = await WeChat.sendAuthRequest('snsapi_userinfo');
+  //     let data = {
+  //       code: codeRes.code,
+  //       app_id: codeRes.appid || 'wx17b69998e914b8f0',
+  //     };
+  //     console.log('codeRes', codeRes)
+  //     const res = await appWechatSignIn(data);
+  //     const signData = {
+  //       ...res.account,
+  //       provider: 'wechat',
+  //     }
+  //     console.log('res', res);
+  //     afterLoginSuccess(res, signData);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
+  //
+  // // 苹果登录的请求
+  // const appleSignIn = async result => {
+  //   const {fullName, identityToken, user, email} = result;
+  //   console.log('result', result);
+  //   try {
+  //     const data = {
+  //       email: email,
+  //       user_id: user,
+  //       identity_token: identityToken,
+  //       nickname: fullName.nickname || fullName.failyName,
+  //       provider: 'apple',
+  //       unionid: user,
+  //     };
+  //     const res = await appAppleSignIn(data);
+  //     afterLoginSuccess(res, data);
+  //   } catch (e) {
+  //     console.log('e', e);
+  //     Toast.showError('苹果登录失败, 请稍后重试。');
+  //   }
+  // };
 
   const afterLoginSuccess = async (res, raw_data = {}) => {
     if (res.error) {
@@ -177,9 +177,8 @@ const JveryfyBottomViewAndroid = ({}) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     width: '100%',
-    height: 100,
+    height: 160,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'black',
@@ -188,46 +187,18 @@ const styles = StyleSheet.create({
   otherLogin: {
     color: '#bdbdbd',
     fontSize: 12,
-
-    // backgroundColor: 'red',
-    // paddingTop: 5,
-    // paddingBottom: 5,
-    // marginBottom: 50,
+    textAlign: 'center',
   },
   thirdLoginWrap: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    width: '100%',
-    // paddingTop: 60,
-    paddingLeft: 80,
-    paddingRight: 80,
-    // backgroundColor: 'black',
-  },
-
-  thirdLogin: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
     // width: '100%',
-    paddingTop: 60,
-    // paddingLeft: 56,
-    // paddingRight: 56,
+    // width: SCREEN_WIDTH,
+    paddingLeft: 60,
+    paddingRight: 80,
     backgroundColor: 'black',
-  },
-  thirdLogoWraper: {
-    width: 40,
-    height: 40,
-    borderRadius: 40,
-    backgroundColor: 'black',
-  },
-  thirdLogo: {
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    width: 40,
-    height: 40,
   },
 });
 
--- src/pages/sessions/one-login.js
+++ src/pages/sessions/one-login.js
@@ -95,13 +95,14 @@ const OneLogin = ({navigation, route}) => {
     // logoW: 100,                             //logo宽
     // logoH: 100,                              //logo高
 
-    numberSize: 20, //手机号码字体大小（单位:sp）
-    numberColor: 16777215, //手机号码字体颜色
+    numberSize: 22, //手机号码字体大小（单位:sp）
+    numberColor: -67333, //手机号码字体颜色
     //为保障显示效果，请同时设置x,y,w,h
-    // numberX: 50,                            //号码栏相对于屏幕左边x轴偏移
-    // numberY: 100,                           //号码栏相对于标题栏下边缘y偏移
-    // numberW: 250,                           //号码栏宽度
-    // numberH: 25,                            //号码栏高度
+    // 0, 40, 200, 18
+    numberX: 0,                            //号码栏相对于屏幕左边x轴偏移
+    numberY: 100,                           //号码栏相对于标题栏下边缘y偏移
+    numberW: 200,                           //号码栏宽度
+    numberH: 18,                            //号码栏高度
 
     sloganHidden: true, //slogan是否隐藏
     sloganTextSize: 16, //slogan字体大小
@@ -123,7 +124,7 @@ const OneLogin = ({navigation, route}) => {
     // 0, 80, 265, 45
     //0, 94, 290, 50
     loginBtnOffsetX: 18,                          //登录按钮相对于屏幕左边x轴偏移
-    loginBtnOffsetY: 145, //登录按钮相对于标题栏下边缘y偏移
+    loginBtnOffsetY: 125, //登录按钮相对于标题栏下边缘y偏移
     loginBtnWidth: 88, //登录按钮宽度
     loginBtnHeight: 15, //180/(1062/70),                          //登录按钮高度
 
@@ -131,20 +132,20 @@ const OneLogin = ({navigation, route}) => {
     privacyTwo: ['《隐私政策》', `${BaseApiUrl}/home/private_policy`], //隐私条款二（显示名称和url，请严格按照格式）
     privacyColor: [0xff00f000, 0xff000000], //隐私条款颜色 （显示名称和url的颜色，请严格按照格式）
     privacyText: ['登录注册即表示已阅读并同意', '和', '、', ''], //隐私条款名称外的文字
-    privacyTextSize: 11, //隐私条款文字字体大小
+    privacyTextSize: 15, //隐私条款文字字体大小
     privacyTextGravityMode: 'center', //隐私条款文本对齐方式，目前仅支持 left、center
     privacyBookSymbolEnable: true, //隐私条款是否显示书名号，默认不显示
     //为保障显示效果，请同时设置x,y,w,h
     //privacyConstraints: [0, 325, 300, 70],
-    privacyOffsetX: 1,                                             //隐私条款相对于屏幕左边x轴偏移
-    privacyOffsetY: 0, //隐私条款相对于授权页面底部下边缘y偏移
-    privacyW: 260, //隐私条款宽度
-    privacyH: 150, //隐私条款高度
+    // privacyOffsetX: 1,                                             //隐私条款相对于屏幕左边x轴偏移
+    // privacyOffsetY: 0, //隐私条款相对于授权页面底部下边缘y偏移
+    privacyW: 200, //隐私条款宽度
+    privacyH: 6, //隐私条款高度
     privacyCheckboxHidden: false, //checkBox是否隐藏，默认不隐藏
     privacyCheckEnable: true, //checkBox默认状态 默认:NO
     privacyCheckedImage: 'checkbox_selected', //checkBox选中时图片
     privacyUncheckedImage: 'checkbox_unselected', //checkBox未选中时图片
-    privacyCheckboxSize: 12, //设置隐私条款checkbox尺寸 默认是10
+    privacyCheckboxSize: 13, //设置隐私条款checkbox尺寸 默认是10
     unAgreePrivacyHintToast: true,
 
     privacyWebNavColor: -16777216, //协议页导航栏背景颜色
@@ -158,7 +159,7 @@ const OneLogin = ({navigation, route}) => {
     {customViewName: 'jverify_bottom_view', customViewPoint: [0, 500, SCREEN_WIDTH, 150]},
   ];
   const customViewParamsAdnroid = [
-    {customViewName: 'jverify_bottom_view_android', customViewPoint: [0, 500, SCREEN_WIDTH - 20, 130]},
+    {customViewName: 'jverify_bottom_view_android', customViewPoint: [0, 450, SCREEN_WIDTH, 150]},
   ];
   // JVerification.preLogin();
   if (Platform.OS === 'android') {
@@ -310,18 +311,25 @@ const OneLogin = ({navigation, route}) => {
 
       {/*<Button*/}
       {/*  title="addLoginCustomConfig"*/}
-      {/*  style={{marginTop: 30}}*/}
+      {/*  style={{marginTop: 40}}*/}
       {/*  onPress={() => {*/}
       {/*    JVerification.clearPreLoginCache()*/}
       {/*    if (Platform.OS === 'android') {*/}
-      {/*      JVerification.addLoginCustomConfig(customUIWithConfigAndroid, customViewParams);*/}
+      {/*      JVerification.addLoginCustomConfig(customUIWithConfigAndroid, customViewParamsAdnroid);*/}
       {/*    } else {*/}
       {/*      JVerification.addLoginCustomConfig(customUIWithConfigiOS, customViewParams);*/}
       {/*    }*/}
       {/*  }}*/}
       {/*/>*/}
 
-      {/*<Button title="login" onPress={() => JVerification.login(false)} />*/}
+      {/*<Button title="login" onPress={() => {*/}
+      {/*  if (Platform.OS === 'android') {*/}
+      {/*    console.log('android');*/}
+      {/*    JVerification.addLoginCustomConfig(customUIWithConfigAndroid, customViewParamsAdnroid);*/}
+      {/*  }*/}
+      {/*  JVerification.login(false)*/}
+      {/*  }*/}
+      {/*} />*/}
 
       {/*<Button*/}
       {/*  title="自定义弹窗授权页"*/}
