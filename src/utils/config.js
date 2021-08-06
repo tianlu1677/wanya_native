45e28784a3011f7b2459d9553a36e8e9a001725b
tianlu1677 <tianlu1677@gmail.com>
2021-08-06 13:23:42 +0800

    update

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,7 @@ export const BaseProApi = 'https://meirixinxue.com/'; //'https://meirixinxue.com
 // export const BaseProApi = 'https://xinxue.meirixinxue.com/'; //'https://meirixinxue.com/';
 // export const BaseDevApi = 'https://xinxue.meirixinxue.com/';
 // export const BaseDevApi = 'http://tl-test.ce04.com/';
-export const BaseDevApi = 'https://meirixinxue.com';
+export const BaseDevApi = 'https://meirixinxue.com/';
 
 export const BaseApiUrl = __DEV__ ? BaseDevApi : BaseProApi;
 
